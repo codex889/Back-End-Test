77d2c9dbf8a5b23258654f4af529aaff9abc8a40
codex889 <pruetsapon.xuz@gmail.com>
2017-01-19 23:22:02 +0700

    update

--- 2/process.php
+++ 2/process.php
@@ -1,17 +1,20 @@
 <?php
 
+$key=$_GET['key'];
+$key=strtolower($key);
 $text=array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-$num=0;
 
-	if($text[$num]==$key){
-		$num++;
-		break;
-	}
-	else{
-		$num++;
+$num=0;
+if($key!=""){
+	while($num<50){
+		if($text[$num]==$key){
+			$num++;
+			break;
+		}
+		else{
+			$num++;
+		}
 	}
 }
-
 echo $num;
 
-?>
