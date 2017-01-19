c34f525714dfcb2a4dbb24221bab3fc7dbc8fc42
codex889 <pruetsapon.xuz@gmail.com>
2017-01-19 22:50:58 +0700

    update

--- 1/process.php
+++ 1/process.php
@@ -1,3 +1,20 @@
 <?php
 
+$text="0000000000000000000000000000000";
+$len=strlen($text);
+
+$row=0;
+$num=2;
+while($len>0){
+	if($row==0){
+		$len=$len-1;
+	}
+	else{
+		$len=$len-($num+1)*2;
+		$num=$num+2;
+	}
+	$row++;
+}
+echo $row;
+
 ?>
