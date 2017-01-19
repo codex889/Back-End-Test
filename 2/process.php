3e96b9c380bc62a82d149c3d1d0e22906c8dfb60
codex889 <pruetsapon.xuz@gmail.com>
2017-01-19 23:14:06 +0700

    new file

---
+++ 2/process.php
@@ -0,0 +1,17 @@
+<?php
+
+$text=array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+$num=0;
+
+	if($text[$num]==$key){
+		$num++;
+		break;
+	}
+	else{
+		$num++;
+	}
+}
+
+echo $num;
+
+?>
